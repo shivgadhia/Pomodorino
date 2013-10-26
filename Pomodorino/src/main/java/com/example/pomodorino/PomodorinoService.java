2c835bb90f621b04d74f16577d3c8abf04267bc5
shiv <shivgadhia@gmail.com>
2013-10-26 16:28:02 +0100

    Hiding a pomodorino upon clicking on it.

--- Pomodorino/src/main/java/com/example/pomodorino/PomodorinoService.java
+++ Pomodorino/src/main/java/com/example/pomodorino/PomodorinoService.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class PomodorinoService extends Service {
 
     private WindowManager windowManager;
-    private ImageView chatHead;
+    private ImageView pomodorino;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -20,14 +21,18 @@ public class PomodorinoService extends Service {
         return null;
     }
 
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
+        showPomodorino();
+    }
 
+    private void showPomodorino() {
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
 
-        chatHead = new ImageView(this);
-        chatHead.setImageResource(R.drawable.floating_pomodorino);
-
+        pomodorino = new ImageView(this);
+        pomodorino.setImageResource(R.drawable.floating_pomodorino);
+        pomodorino.setOnClickListener(dismissPomodorinoListener);
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
@@ -39,13 +44,26 @@ public class PomodorinoService extends Service {
         params.x = 0;
         params.y = 100;
 
-        windowManager.addView(chatHead, params);
+        windowManager.addView(pomodorino, params);
     }
+
+
+    private View.OnClickListener dismissPomodorinoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            stopSelf();
+        }
+    };
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (chatHead != null) windowManager.removeView(chatHead);
+        hidePomodorino();
+    }
+
+    private void hidePomodorino() {
+        if (pomodorino != null) windowManager.removeView(pomodorino);
     }
 
 
